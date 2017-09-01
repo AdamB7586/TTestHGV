1a50cffda188deab88a786319e39603b023fdebf
Adam Binnersley <abinnersley@gmail.com>
2017-09-01 16:13:03 +0100

    Initial commit

--- src/LearnTest.php
+++ src/LearnTest.php
@@ -3,5 +3,10 @@
 namespace TheoryTest\HGV;
 
 class LearnTest extends \TheoryTest\Car\LearnTest{
+    protected $testType = 'hgv';
+    
+    public $questionsTable = 'hgv_questions';
+    public $progressTable = 'hgv_progress';
+    public $dsaCategoriesTable = 'hgv_sections';
     
 }
--- src/TheoryTest.php
+++ src/TheoryTest.php
@@ -3,5 +3,19 @@
 namespace TheoryTest\HGV;
 
 class TheoryTest extends \TheoryTest\Car\TheoryTest{
+    protected $seconds = 5400;
+    protected $section = 'aditheory';
     
+    protected $audioLocation = '/audio/hgv';
+    
+    public $questionsTable = 'hgv_questions';
+    public $progressTable = 'hgv_test_progress';
+    public $dsaCategoriesTable = 'hgv_dsa_sections';
+    
+    protected static $testType = 'hgv';
+    
+    public function __construct(Database $db, Smarty $layout, User $user, $userID = false) {
+        parent::__construct($db, $layout, $user, $userID);
+        $this->setImagePath(ROOT.DS.'images'.DS.'hgv'.DS);
+    }
 }
