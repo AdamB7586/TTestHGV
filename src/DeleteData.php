3d41447e8546f65349b01f85ee1c796e333e4205
Adam Binnersley <abinnersley@gmail.com>
2017-09-18 15:28:18 +0100

    Add ability to delete HGV user data

---
+++ src/DeleteData.php
@@ -0,0 +1,7 @@
+<?php
+namespace TheoryTest\ADI;
+
+class DeleteData extends \TheoryTest\Car\DeleteData{
+    public $learningProgressTable = 'hgv_progress';
+    public $progressTable = 'hgv_test_progress';
+}
