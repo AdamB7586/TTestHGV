d7a51a5954b5a046b23f68ea383f6af0c4f562b8
Adam Binnersley <abinnersley@gmail.com>
2020-10-28 17:03:52 +0000

    Update TheoryTest.php

--- src/TheoryTest.php
+++ src/TheoryTest.php
@@ -17,6 +17,16 @@ class TheoryTest extends \TheoryTest\Car\TheoryTest
     
     protected $testType = 'hgv';
     
+    /**
+     * Connects to the database sets the current user and gets any user answers
+     * @param Database $db This needs to be an instance of the database class
+     * @param Config $config This needs to be an instance of the Configuration class
+     * @param Smarty $layout This needs to be an instance of the Smarty Template class
+     * @param object $user This should be the user class used
+     * @param int|false $userID If you want to emulate a user set the user ID here
+     * @param string|false $templateDir If you want to change the template location set this location here
+     * @param string $theme This is the template folder to look at currently either 'bootstrap' or 'bootstrap4'
+     */
     public function __construct(Database $db, Config $config, Smarty $layout, $user, $userID = false, $templateDir = false, $theme = 'bootstrap')
     {
         parent::__construct($db, $config, $layout, $user, $userID, $templateDir, $theme);
