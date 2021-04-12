6d0c7221dfc07944ae4ba6c0bf684d48c32f5db4
Adam Binnersley <abinnersley@gmail.com>
2021-04-12 15:09:49 +0100

    Fix HGV to not look at band no like in ADI test

--- src/TheoryTest.php
+++ src/TheoryTest.php
@@ -70,4 +70,47 @@ class TheoryTest extends \TheoryTest\ADI\TheoryTest
             $this->testName = 'HGV Test '.$this->getTest();
         }
     }
+    
+    /**
+     * Marks the current test for the user
+     * @param int|false $time The time to set as taken for the current test of false to not update
+     * @return void Nothing is returned
+     */
+    protected function markTest($time = false)
+    {
+        $this->getQuestions();
+        foreach ($this->questions as $prim) {
+            if ($_SESSION['test'.$this->getTest()][$this->questionNo($prim)]['status'] == 4) {
+                $type = 'correct';
+            } else {
+                $type = 'incorrect';
+            }
+             
+            $dvsa = $this->getDSACat($prim);
+            $this->testresults['dvsa'][$dvsa][$type] = (int)$this->testresults['dvsa'][$dvsa][$type] + 1;
+        }
+        
+        $this->testresults['correct'] = $this->numCorrect();
+        $this->testresults['incorrect'] = ($this->numQuestions() - $this->numCorrect());
+        $this->testresults['incomplete'] = $this->numIncomplete();
+        $this->testresults['flagged'] = $this->numFlagged();
+        $this->testresults['numquestions'] = $this->numQuestions();
+        $this->testresults['percent']['correct'] = round(($this->testresults['correct'] / $this->testresults['numquestions']) * 100);
+        $this->testresults['percent']['incorrect'] = round(($this->testresults['incorrect'] / $this->testresults['numquestions']) * 100);
+        $this->testresults['percent']['flagged'] = round(($this->testresults['flagged'] / $this->testresults['numquestions']) * 100);
+        $this->testresults['percent']['incomplete'] = round(($this->testresults['incomplete'] / $this->testresults['numquestions']) * 100);
+        if ($this->numCorrect() >= $this->passmark) {
+            $this->testresults['status'] = 'pass';
+            $status = 1;
+        } else {
+            $this->testresults['status'] = 'fail';
+            $status = 2;
+        }
+        if ($time !== false) {
+            list($hours, $mins, $secs) = explode(':', $time);
+            $newtime = gmdate('H:i:s', ($this->seconds - (($hours * 60 * 60) + ($mins * 60) + $secs)));
+            $this->userProgress['time_taken'] = $newtime;
+        }
+        $this->db->update($this->progressTable, array_merge(['status' => $status, 'results' => serialize($this->testresults), 'complete' => date('Y-m-d H:i:s'), 'totalscore' => $this->numCorrect()], ($time !== false ? ['time_taken' => $newtime] : [])), ['user_id' => $this->getUserID(), 'test_id' => $this->getTest(), 'status' => 0, 'current_test' => 1]);
+    }
 }
